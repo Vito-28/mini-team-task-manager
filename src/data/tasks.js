c6fd23a8cb29dec91f7eed903f89fdbc9ba7de2c
Vito-28 <vito.chianese@outlook.it>
2026-07-17 20:27:40 +0200

    feat: create initial tasks api structure

---
+++ src/data/tasks.js
@@ -0,0 +1,12 @@
+export const tasks = [
+    {
+        id:1,
+        title:"Study Express",
+        completed:false
+    },
+    {
+        id:2,
+        title:"Learn Node",
+        completed:true
+    }
+];
