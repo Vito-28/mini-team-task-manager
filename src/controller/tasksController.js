db4f1d747a2b7a47395125e77f9d6f96de34c7cb
Vito-28 <vito.chianese@outlook.it>
2026-08-05 21:06:03 +0200

    refactor: add try/catch to controllers

--- src/controller/tasksController.js
+++ src/controller/tasksController.js
@@ -1,48 +1,83 @@
 import { getAllTasks, getTaskById, createTaskFromTitle, updateTaskByIdAndTitle, deleteTaskById } from "../service/tasksService.js";
 
 export const getTasks = async (req, res) => {
-    const tasks = await getAllTasks();
-    res.status(200).json(tasks);
+
+    try {
+        const tasks = await getAllTasks();
+        res.status(200).json(tasks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+    
 };
 
 export const getTask = async (req, res) => {
-    const {id} = req.params;
-    const task = await getTaskById(id);
 
-    if(!task) {
-        return res.status(404).send("Task not found");
+    try {
+        const {id} = req.params;
+        const task = await getTaskById(id);
+
+        if(!task) {
+            return res.status(404).send("Task not found");
+        }
+
+        res.status(200).json(task);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");        
     }
 
-    res.status(200).json(task);
 };
 
 export const createTask = async (req, res) => {
-    const {title} = req.body;
-    
-    const task = await createTaskFromTitle(title);
-    res.status(201).json(task);
+
+    try {
+        const {title} = req.body;
+        
+        const task = await createTaskFromTitle(title);
+        res.status(201).json(task);        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");         
+    }
+
 };
 
 export const updateTask = async (req, res) => {
-    const {id} = req.params;
-    const {title} = req.body;
 
-    const task = await updateTaskByIdAndTitle(id, title);
+    try {
+        const {id} = req.params;
+        const {title} = req.body;
 
-    if(!task){
-        return res.status(404).send("Task not found");
+        const task = await updateTaskByIdAndTitle(id, title);
+
+        if(!task){
+            return res.status(404).send("Task not found");
+        }
+
+        res.status(200).json(task);        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");         
     }
-
-    res.status(200).json(task);
+    
 };
 
 export const deleteTask = async (req, res) => {
-    const {id} = req.params;
-    const task = await deleteTaskById(id);
 
-    if(!task){
-        return res.status(404).send("Task not found");
+    try {
+        const {id} = req.params;
+        const task = await deleteTaskById(id);
+
+        if(!task){
+            return res.status(404).send("Task not found");
+        }
+
+        res.status(204).end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");        
     }
 
-    res.status(204).end();
 };
