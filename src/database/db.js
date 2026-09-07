574de0774d867f0cb8109ecd797c8d924bf54770
Vito-28 <vito.chianese@outlook.it>
2026-09-07 20:44:09 +0200

    feat: add PostgreSQL database connection

---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'task_manager',
+  password: 'root',
+  port: 5432,
+});
+
+export default pool;
