4a8c5ee118dcf2a465638b08890d9073e7903ceb
Vito-28 <vito.chianese@outlook.it>
2026-09-24 19:27:59 +0200

    feat: implement jwt authorization middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,12 +1,30 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
 const authorization = (req, res, next) => {
 
-    const token = req.headers.authorization?.split(" ")[1];
+    const authHeader = req.headers['authorization'];
 
-    if (token !== "secret123") {
-        return res.status(401).send("Unauthorized");
+    if(authHeader && authHeader.split(' ')[0] !== 'Bearer') {
+        return res.status(401).json({ message: 'Access denied. No Bearer in Authorization header.' });
     }
 
-    next();
+    const token = authHeader && authHeader.split(' ')[1]; // Expects: Bearer <token>
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {
+            id: decoded.sub,
+            name: decoded.name
+        }
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid or expired token.' });
+    }
 
 };
 
